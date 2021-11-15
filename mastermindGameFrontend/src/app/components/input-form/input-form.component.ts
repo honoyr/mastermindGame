25127ea5bb9bd4c53e54a356f5451598f08b411f
Dennis G <33399226+honoyr@users.noreply.github.com>
2021-11-15 04:05:49 -0800

    add validators to form

--- mastermindGameFrontend/src/app/components/input-form/input-form.component.ts
+++ mastermindGameFrontend/src/app/components/input-form/input-form.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
+import {Component, Input, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {GameSettingsDto} from "../../model/GameSettings";
 
 @Component({
   selector: 'app-input-form',
@@ -8,16 +9,36 @@ import {FormControl, FormGroup} from "@angular/forms";
 })
 export class InputFormComponent implements OnInit {
 
+
+  @Input()
+  gameSettings$!: GameSettingsDto;
+
   form!: FormGroup;
+  min: string = '1';
+  max: string = '8'
+  maxLen: string = '5'
+  regex: string = `[${this.min}-${this.max}]{${this.maxLen}}`
   constructor() { }
 
   ngOnInit(): void {
     this.form = new FormGroup({
-      guess: new FormControl('',)
+      guess: new FormControl('',
+        [
+                      // Validators.maxLength(4),
+                      // Validators.minLength(4),
+                      // Validators.max(7),
+                      // Validators.min(0),
+                      Validators.pattern(this.regex),
+                      Validators.required,
+                      ],  )
     })
   }
 
+  get guess() { return this.form.get('guess'); }
+
   submit() {
     console.log(this.form)
+    const formData = {...this.form.value};
+    console.log(formData)
   }
 }
